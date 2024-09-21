c7e34e3663532ef35c88084feec93cada61110f1
jawad0098 <hassanjawad858@gmail.com>
2024-09-21 22:19:44 +0500

    comment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li><link href="/" /></li>
+        <li><link href="./nav" /></li>
+
+      </ul>
+    </div>
+  )
+}
+
+export default navbar
